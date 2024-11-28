9311a45c2e84166ab7dcec4bd91c3c64cf0cf10f
Emiliya Naydenova <emilia_n2@yahoo.com>
2024-11-28 19:45:02 +0200

    CI/CD pipeline workflow

--- tests/unit/test_aws_essentials_exam_stack.py
+++ tests/unit/test_aws_essentials_exam_stack.py
@@ -7,7 +7,7 @@ from aws_essentials_exam.aws_essentials_exam_stack import AwsEssentialsExamStack
 # resource in aws_essentials_exam/aws_essentials_exam_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = AwsEssentialsExamStack(app, "aws-essentials-exam")
+    stack = AwsEssentialsExamStack(app, "aws_essentials_exam")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
@@ -18,6 +18,6 @@ def test_sqs_queue_created():
 
 def test_stack():
     app = core.App()
-    stack = AwsEssentialsExamStack(app, "aws-essentials-exam")
-    template = app.synth().get_stack_by_name("aws-essentials-exam").template
+    stack = AwsEssentialsExamStack(app, "aws_essentials_exam")
+    template = app.synth().get_stack_by_name("aws_essentials_exam").template
     assert "AWS::S3::Bucket" in template["Resources"]
